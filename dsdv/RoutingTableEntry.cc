86e41a33576087621a52e8670fd3467f487eb07b
prabhdepSP <prabhcoder@gmail.com>
2020-01-20 22:38:05 +0530

    fixed typos in RountingTableEntry.cc

--- dsdv/RoutingTableEntry.cc
+++ dsdv/RoutingTableEntry.cc
@@ -1,36 +1,29 @@
-#include <RoutingTableEntry.h>
-
-class RoutingTableEntry{
-	Public:
-		
-		RoutingTableEntry(ns3::Ipv4Address destinationIp = ns3::Ipv4Address() ,
-				uint32_t seq_number = 0,
-				uint32_t hops_count = 0,
-				ns3::Ptr<ns3::NetDevice> dev = 0, ns3::Ipv4InterfaceAddress output_link_interface= ns3::Ipv4InterfaceAddress()){
-			dstIp = destinationIp;
-			seqNumber = seq_number;
-			hopsCount = hops_count;
-			device = dev;
-			outputLinkInterface = output_link_interface;}
-		~RoutingTableEntry(){
-		}
-		RoutingTableEntry{
-		}	
-		uint32_t getSeqNumber(){
-			return this->seqNumber;
-		}
-		void setSeqNumber(uint32_t seq_number){
-			this->seqNumber = seq_number;
-		}
-		uint32_t getHopsCount(){
-			return this->hopsCount;
-		}
-		void setHopsCount(uint32_t hops_count){
-			this->hopsCount = hops_count;
-		}
-		ns3::Ipv4Address getDsptIp(){
-			return this->dstIp;
-		}
-};
+#include "RoutingTableEntry.h"
+RoutingTableEntry::RoutingTableEntry(ns3::Ipv4Address destinationIp,uint32_t seq_number,uint32_t hops_count,ns3::Ptr<ns3::NetDevice> dev, ns3::Ipv4InterfaceAddress output_link_interface){
+	dstIp = destinationIp;
+	seqNumber = seq_number;
+	hopsCount = hops_count;
+	device = dev;
+	outputLinkInterface = output_link_interface;
+}
+RoutingTableEntry::~RoutingTableEntry(){
+}
+RoutingTableEntry::RoutingTableEntry(){
+}	
+uint32_t RoutingTableEntry:: getSeqNumber(){
+	return this->seqNumber;
+}
+void RoutingTableEntry:: setSeqNumber(uint32_t seq_number){
+	this->seqNumber = seq_number;
+}
+uint32_t RoutingTableEntry::getHopsCount(){
+	return this->hopsCount;
+}
+void RoutingTableEntry::setHopsCount(uint32_t hops_count){
+	this->hopsCount = hops_count;
+}
+ns3::Ipv4Address RoutingTableEntry::getDsptIp(){
+	return this->dstIp;
+}
 
 
