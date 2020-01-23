1cbd08e5475a9f248a26a20a96bc05156311fa07
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-01-23 15:08:29 +0530

    Updated Routing Table Entry Implementation

--- dsdv/RoutingTableEntry.cc
+++ dsdv/RoutingTableEntry.cc
@@ -1,35 +1,75 @@
 #include "RoutingTableEntry.h"
 
-RoutingTableEntry::RoutingTableEntry(ns3::Ipv4Address ip,uint32_t seq_num,uint32_t hops_count,ns3::Ptr<ns3::NetDevice> dev, ns3::Ipv4InterfaceAddress output_link_interface):dstIp(ip),seqNumber(seq_num),hopsCount(hops_count),device(dev),outputLinkInterface(output_link_interface){}
-
-RoutingTableEntry::~RoutingTableEntry(){
-}
+RoutingTableEntry::RoutingTableEntry(ns3::Ipv4Address destIp,uint32_t seqNumber,uint32_t hops,ns3::Ipv4InterfaceAddress interface,ns3::Ipv4Address nextHop,ns3::Time lifetime,ns3::Time settlingTime,bool changed,ns3::Ptr<ns3::NetDevice> dev):
+	dstIp(destIp),
+	seqNumber(seqNumber),
+	hopsCount(hops),
+	outputLinkInterface(interface),
+	changed(changed),
+	lifeTime(lifetime),
+	settlingTime(settlingTime){
+		route = ns3::Create<ns3::Ipv4Route>();
+		route->SetGateway(nextHop);
+		route->SetDestination(dstIp);
+		route->SetSource(outputLinkInterface.GetLocal());
+		route->SetOutputDevice(dev);
+	}
 RoutingTableEntry::RoutingTableEntry(){
 }	
-uint32_t RoutingTableEntry:: getSeqNumber() const{
+inline uint32_t RoutingTableEntry:: getSeqNumber() const{
 	return this->seqNumber;
+}
+inline uint32_t RoutingTableEntry::getHopsCount() const{
+	return this->hopsCount;
+}
+inline ns3::Ptr<ns3::NetDevice> RoutingTableEntry::getDevice() const{
+	return route->GetOutputDevice();
+}
+inline ns3::Ipv4InterfaceAddress RoutingTableEntry::getLink() const{
+	return outputLinkInterface;
+}
+inline ns3::Ipv4Address RoutingTableEntry::getDsptIp() const{
+	return this->dstIp;
+}
+inline ns3::Ptr<ns3::Ipv4Route> RoutingTableEntry::getRoute() const{
+	return route;
+}
+inline bool RoutingTableEntry::isChanged() const{
+	return changed;
+}
+inline ns3::Time RoutingTableEntry::getLifeTime() const{
+	return ns3::Simulator::Now() - lifeTime;
+}
+inline ns3::Time RoutingTableEntry::getSettlingTime() const{
+	return settlingTime;
+}
+inline ns3::Ipv4Address RoutingTableEntry::getNextHop() const{
+	return route->GetGateway();
 }
 void RoutingTableEntry:: setSeqNumber(uint32_t seq_number){
 	this->seqNumber = seq_number;
 }
-uint32_t RoutingTableEntry::getHopsCount() const{
-	return this->hopsCount;
-}
 void RoutingTableEntry::setHopsCount(uint32_t hops_count){
 	this->hopsCount = hops_count;
 }
-ns3::Ipv4Address RoutingTableEntry::getDsptIp() const{
-	return this->dstIp;
-}
-ns3::Ptr<ns3::NetDevice> RoutingTableEntry::getDevice() const{
-	return device;
-}
 void RoutingTableEntry::setDevice(ns3::Ptr<ns3::NetDevice> dev){
-	device = dev;
-}
-ns3::Ipv4InterfaceAddress RoutingTableEntry::getLink() const{
-	return outputLinkInterface;
+	route->SetOutputDevice(dev);
 }
 void RoutingTableEntry::setLink(ns3::Ipv4InterfaceAddress link){
 	outputLinkInterface = link;
+}
+inline void RoutingTableEntry::setChanged(bool flag){
+	changed = flag;
+}
+inline void RoutingTableEntry::setLifeTime(ns3::Time time){
+	lifeTime = time;
+}
+inline void RoutingTableEntry::setSettlingTime(ns3::Time time){
+	settlingTime = time;
+}
+inline void RoutingTableEntry::setNextHop(ns3::Ipv4Address hop){
+	route->SetGateway(hop);
+}
+inline void RoutingTableEntry::setRoute(ns3::Ptr<ns3::Ipv4Route> route){
+	this->route = route;
 }
--- dsdv/RoutingTableEntry.h
+++ dsdv/RoutingTableEntry.h
@@ -3,6 +3,7 @@
 #include <ns3/nstime.h>
 #include <ns3/net-device.h>
 #include <ns3/ipv4-route.h>
+#include <ns3/simulator.h>
 class RoutingTableEntry{
 	private: 
 		ns3::Ipv4Address dstIp;
@@ -14,31 +15,31 @@ class RoutingTableEntry{
 		ns3::Time lifeTime;
 		ns3::Time settlingTime;
 	public:
-		RoutingTableEntry(ns3::Ipv4Address ip = ns3::Ipv4Address() ,uint32_t seq_num= 0,uint32_t hops_count = 0,ns3::Ptr<ns3::NetDevice> dev= 0, ns3::Ipv4InterfaceAddress outputLinkInterface = ns3::Ipv4InterfaceAddress());
+		RoutingTableEntry(ns3::Ipv4Address destIp,uint32_t seqNumber,uint32_t hops,ns3::Ipv4InterfaceAddress interface,ns3::Ipv4Address nextHop,ns3::Time lifetime,ns3::Time settlingTime,bool changed,ns3::Ptr<ns3::NetDevice> dev);
 		~RoutingTableEntry();
 		RoutingTableEntry();
 		inline uint32_t getSeqNumber() const;
-		inline void setSeqNumber(uint32_t seq_number);
 		inline uint32_t getHopsCount() const;
-		inline void setHopsCount(uint32_t hops_count);
-		inline ns3::Ipv4Address getDsptIp() const;
 		inline ns3::Ptr<ns3::NetDevice> getDevice() const;
-		inline void setDevice(ns3::Ptr<ns3::NetDevice> dev);
+		inline ns3::Ipv4Address getDsptIp() const;
 		inline ns3::Ipv4InterfaceAddress getLink() const;
+		inline ns3::Ptr<ns3::Ipv4Route> getRoute() const;
+		inline bool isChanged() const;
+		inline ns3::Time getLifeTime() const;
+		inline ns3::Time getSettlingTime() const;
+		inline ns3::Ipv4Address getNextHop() const;
+		inline void setSeqNumber(uint32_t seq_number);
+		inline void setHopsCount(uint32_t hops_count);
+		inline void setDevice(ns3::Ptr<ns3::NetDevice> dev);
 		inline void setLink(ns3::Ipv4InterfaceAddress link);
-		inline bool isChanged();
 		inline void setChanged(bool flag);
-		inline ns3::Time getLifeTime();
 		inline void setLifeTime(ns3::Time time);
-		inline ns3::Time getSettlingTime();
 		inline void setSettlingTime(ns3::Time time);
 		bool operator==(const RoutingTableEntry& entry){
 			return dstIp == entry.dstIp;
 		}
-		inline void setNextHop(ns3::Ipv4Address hop);
-		inline ns3::Ipv4Address getNextHop();
+		inline void setNextHop(ns3::Ipv4Address hop) ;
 		inline void setRoute(ns3::Ptr<ns3::Ipv4Route> route);
-		inline ns3::Ptr<ns3::Ipv4Route> getRoute();
 
 };
 
