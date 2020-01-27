35c5bb7add804f105b6fc8674438004dfba1f023
prabhdepSP <prabhcoder@gmail.com>
2020-01-27 18:18:50 +0530

    initial routing protocol with notify

--- dsdv/RoutingProtocol.h
+++ dsdv/RoutingProtocol.h
@@ -8,20 +8,21 @@ class RoutingProtocol:public ns3::Ipv4RoutingProtocol{
 		RoutingTable routingTable;
 		std::map<ns3::Ptr<ns3::Socket>,ns3::Ipv4InterfaceAddress> socketToInterfaceMap;
 	private:
-		void start();
-		ns3::Ptr<ns3::Socket> findSocketWithInterfaceAddress(ns3::Ipv4InterfaceAddress ) const;			
-		void recvDsdv(ns3::Ptr<ns3::Socket>);
-		void drop(ns3::Ptr<const ns3::Packet>, const ns3::Ipv4Header &, ns3::Socket::SocketErrno);
-		void send(ns3::Ptr<ns3::Ipv4Route>, ns3::Ptr<const ns3::Packet>, const ns3::Ipv4Header &);
+		void start(); //not implemented
+		ns3::Ptr<ns3::Socket> findSocketWithInterfaceAddress(ns3::Ipv4InterfaceAddress ) const;	//implemented
+		void recvDsdv(ns3::Ptr<ns3::Socket>); //not implemented
+		void drop(ns3::Ptr<const ns3::Packet>, const ns3::Ipv4Header &, ns3::Socket::SocketErrno); //not implemented
+		void send(ns3::Ptr<ns3::Ipv4Route>, ns3::Ptr<const ns3::Packet>, const ns3::Ipv4Header &); //not implemented
 
-	public:
-		
-   		static ns3::TypeId GetTypeId (void);
+	public:	
+		RoutingProtocol();//not implemented
+		~RoutingProtocol();//not implemented
+   		static ns3::TypeId GetTypeId (void);//extra
   		static const uint32_t DSDV_PORT;
-		virtual void notifyAddAddress(uint32_t interface, ns3::Ipv4InterfaceAddress) const;
-		virtual void notifyRemoveAddress(uint32_t interface, ns3::Ipv4InterfaceAddress) const;
-		virtual void notifyInterfaceUp(uint32_t );
-		virtual void notifyInterfaceDown(uint32_t );
-		virtual void setIpv4(ns3::Ptr<ns3::Ipv4>);
-
+		virtual void NotifyAddAddress(uint32_t interface,ns3::Ipv4InterfaceAddress); //implemented
+		virtual void NotifyRemoveAddress(uint32_t interfaceNo, ns3::Ipv4InterfaceAddress);//implemented
+		virtual void NotifyInterfaceUp(uint32_t );//implemented
+		virtual void NotifyInterfaceDown(uint32_t );//implemented
+		virtual void SetIpv4(ns3::Ptr<ns3::Ipv4>);//not implemented
+  		bool RouteInput (ns3::Ptr<const ns3::Packet> p, const ns3::Ipv4Header &header, ns3::Ptr<const ns3::NetDevice> idev, UnicastForwardCallback ucb,MulticastForwardCallback mcb, LocalDeliverCallback lcb, ErrorCallback ecb);
 };
--- dsdv/prabhdeeprp.cc
+++ dsdv/prabhdeeprp.cc
@@ -2,15 +2,15 @@
 #include "ns3/uinteger.h"
 #include "ns3/udp-socket-factory.h"
 /*static ns3::TypeId GetTypeId ()
-{
-	static ns3::TypeId tid = ns3::TypeId ("ns3::dsdv::DeferredRouteOutputTag")
-		.SetParent<ns3::Tag> ()
-		.SetGroupName ("Dsdv")
-		.AddConstructor<DeferredRouteOutputTag> ()
-		;
-	return tid;
-}
-*/
+  {
+  static ns3::TypeId tid = ns3::TypeId ("ns3::dsdv::DeferredRouteOutputTag")
+  .SetParent<ns3::Tag> ()
+  .SetGroupName ("Dsdv")
+  .AddConstructor<DeferredRouteOutputTag> ()
+  ;
+  return tid;
+  }
+  */
 //******dsdv_port //EMPTY
 ns3::Ptr<ns3::Socket> RoutingProtocol::findSocketWithInterfaceAddress(ns3::Ipv4InterfaceAddress interface) const{
 	auto itr = socketToInterfaceMap.cbegin();
@@ -38,6 +38,49 @@ void RoutingProtocol::notifyInterfaceUp(uint32_t interface){
 	socketToInterfaceMap.insert (std::make_pair (socket,iface));
 	ns3::Ptr<ns3::NetDevice> dev = ptrIp->GetNetDevice (ptrIp->GetInterfaceForAddress (iface.GetLocal ()));
 	/*inclomplete getlocal get net device"*/
+}
+void RoutingProtocol::notifyAddAddress(uint32_t interfaceNo){
+	if (!ptrIp->IsUp (interfaceNo))
+	{
+		return;
+	}
+	ns3::Ipv4InterfaceAddress iface = ptrIp->GetAddress (interfaceNo,0);
+	ns3::Ptr<ns3::Socket> socket = RoutingProtocol::findSocketWithInterfaceAddress (iface);
+	if (!socket)
+	{
+		if (iface.GetLocal () == ns3::Ipv4Address ("127.0.0.1"))
+		{
+			return;
+		}
+		ns3::Ptr<ns3::Socket> socket = ns3::Socket::CreateSocket (GetObject<ns3::Node> (),ns3::UdpSocketFactory::GetTypeId ());
+		/*imortant 		//socket->SetRecvCallback (MakeCallback (&RoutingProtocol::RecvDsdv,this));     */
+		socket->BindToNetDevice (ptrIp->GetNetDevice (interfaceNo));
+		socket->Bind (ns3::InetSocketAddress (ns3::Ipv4Address::GetAny (), DSDV_PORT));
+		socket->SetAllowBroadcast (true);
+		socketToInterfaceMap.insert (std::make_pair (socket,iface));
+		ns3::Ptr<ns3::NetDevice> dev = ptrIp->GetNetDevice (ptrIp->GetInterfaceForAddress (iface.GetLocal ()));
+		//important		RoutingTableEntry rt (/*device=*/ dev, /*dst=*/ iface.GetBroadcast (),/*seqno=*/ 0, /*iface=*/ iface,/*hops=*/ 0,
+		//				/*next hop=*/ iface.GetBroadcast (), /*lifetime=*/ ns3::Simulator::GetMaximumSimulationTime ());   
+		routingTable.addRouteEntry (rt);
+	}
+}
+void RoutingProtocol::notifyRemoveAddress(uint32_t interfaceNo, ns3::Ipv4InterfaceAddress ifaceAddress){
+	ns3::Ptr<ns3::Socket> socket = findSocketWithInterfaceAddress(ifaceAddress);
+	if(socket != NULL){
+		socketToInterfaceMap.erase (socket);
+		if (ptrIp->GetNAddresses (interfaceNo)){	
+			ns3::Ipv4InterfaceAddress iface = ptrIp->GetAddress(interfaceNo,0);
+			ns3::Ptr<ns3::Socket> socket = ns3::Socket::CreateSocket (GetObject<ns3::Node> (),ns3::UdpSocketFactory::GetTypeId ());
+			socket->SetRecvCallback (MakeCallback (&RoutingProtocol::recvDsdv,this));
+			socket->Bind (ns3::InetSocketAddress (ns3::Ipv4Address::GetAny (), DSDV_PORT));
+			socket->SetAllowBroadcast (true);
+			socketToInterfaceMap.insert (std::make_pair (socket,iface));
+		}
+	}
+}
+bool RoutingProtocol::RouteInput (ns3::Ptr<const ns3::Packet> p, const ns3::Ipv4Header &header, ns3::Ptr<const ns3::NetDevice> idev, UnicastForwardCallback ucb,MulticastForwardCallback mcb, LocalDeliverCallback lcb, ErrorCallback ecb){
+	if(socketToInterfaceMap.empty()) return false;
+
+}
 
 
-}	
