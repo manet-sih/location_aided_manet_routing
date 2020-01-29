3008059c44183c7ac48272f3039a42571602f7a2
Sachin <sachin9027328196@gmail.com>
2020-01-30 00:24:34 +0530

    intial route input

--- dsdv/routing_protocol_sachin.cc
+++ dsdv/routing_protocol_sachin.cc
@@ -1,4 +1,6 @@
+#include "ns3/ipv4-l3-protocol.h"
 #include "RoutingProtocol.h"
+#include "ns3/packet.h"
 bool RoutingProtocol::RouteInput (ns3::Ptr<const ns3:: Packet> p,
                               const ns3::Ipv4Header &header,
                               ns3::Ptr<const ns3::NetDevice> idev,
@@ -15,11 +17,7 @@ bool RoutingProtocol::RouteInput (ns3::Ptr<const ns3:: Packet> p,
           if( src == (itr->second).GetLocal())
             return true;
       }
-      for(auto itr =socketToInterfaceMap.begin();itr!=socketToInterfaceMap.end();itr++)
-      {
-          if( src == (itr->second).GetLocal())
-            return true;
-      }
+      
       RoutingTableEntry rte;
       
      
@@ -30,19 +28,68 @@ bool RoutingProtocol::RouteInput (ns3::Ptr<const ns3:: Packet> p,
        if (routingTable.search(toDst.getNextHop(),ne))
          {
            ns3::Ptr<ns3::Ipv4Route> route = ne.getRoute ();
-           NS_LOG_LOGIC (mainAddress << " is forwarding packet " << p->GetUid ()
-                                       << " to " << dst
-                                       << " from " << header.GetSource ()
-                                       << " via nexthop neighbor " << toDst.getNextHop ());
+       
            ucb (route,p,header);
            return true;
          }
-     } 
-      else
+          else
       { //we will direct this packet for interzone processsing
       }
       
+     } 
+     
 
      
       
- }   
+}
+ns3::Ptr<ns3::Ipv4Route> RoutingProtocol::RouteOutput (ns3::Ptr<ns3::Packet> p, const ns3::Ipv4Header &header, ns3::Ptr<ns3::NetDevice> oif,ns3::Socket::SocketErrno &sockerr)
+{
+   if (!p)
+     {
+       return RoutingProtocol::LoopbackRoute(header,oif);
+     }
+     if (socketToInterfaceMap.empty ())
+     {
+       
+       ns3::Ptr<ns3::Ipv4Route> route;
+       return route;
+     }
+      RoutingTableEntry rt;
+      ns3::Ipv4Address dst = header.GetDestination ();
+      if( routingTable.search(header.GetDestination (),rt))
+      {
+         ns3::Ptr<ns3::Ipv4Route> route=rt.getRoute();
+          if(rt.getHopsCount()==1)
+            {
+               
+              if (oif != 0 && route->GetOutputDevice () != oif)
+                  return ns3::Ptr<ns3::Ipv4Route>();
+            }
+            return route;
+      }
+      else{
+             RoutingTableEntry rti;
+            if(routingTable.search(rt.getNextHop(),rti))
+            {
+               ns3::Ptr<ns3::Ipv4Route> route=rti.getRoute();
+               if (oif != 0 && route->GetOutputDevice () != oif)
+               {
+                 return ns3::Ptr<ns3::Ipv4Route>();
+               }
+               return route;
+            }
+             
+             
+            
+      }
+     return LoopbackRoute (header,oif);
+
+}
+void RoutingProtocol::send(ns3::Ptr<ns3::Ipv4Route>route, ns3::Ptr<const ns3::Packet>packet, const ns3::Ipv4Header &header)
+{
+    ns3::Ptr<ns3::Ipv4L3Protocol> l3 =RoutingProtocol::ptrIp->GetObject<ns3::Ipv4L3Protocol> ();
+   NS_ASSERT (l3 != 0);
+   ns3::Ptr<ns3::Packet> p = packet->Copy ();
+   l3->Send (p,route->GetSource (),header.GetDestination (),header.GetProtocol (),route);
+}
+  
